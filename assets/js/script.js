9f26e563349eccce600d29961da0b9c14a8dbfa8
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-07-02 22:50:30 -0500

    search city function console log working

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// 1)Create the search function
+// 2)display current city conditions
+//      name, current date, icon representative for current weather, temperature, humidity, wind speed, and UV index with color indication
+//      future weather conditions for that city
+// 3)5 day forecast
+//      date, icon rep of weather, temp, wind speed, humidity
+//4)city goes in history and is clickable
+
+
+var getCityWeather = function() {
+    console.log("function was called");
+};
+
+getCityWeather();
