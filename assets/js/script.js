c18f7356e73de831dec53b8735bdd74dab4ca9ae
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-07-03 23:50:33 -0500

    switched to geo and one call and displayed all but icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#city");
-var cityContainerEl = document.querySelector("#city-container");
+var cityContainerEl = document.getElementById("city-container");
 var citySearchTerm = document.querySelector("#city-search-term");
-
+var currentTempEl= document.querySelector("#currentTemp");
+var newCitySearchTerm= "Brookfield"
 
 // 1)Create the search function
 // 2)display current city conditions
@@ -15,16 +16,18 @@ var citySearchTerm = document.querySelector("#city-search-term");
 
 
 // Fetch the api
-var getCityWeather = function(city) {
+var getCityWeather = function(lon,lat,city) {
     // format the weather api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=526fc11b10a27117543151b1de2f92b6";
-
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon +"&units=imperial&appid=526fc11b10a27117543151b1de2f92b6";
+    console.log(city);
+    console.log(lat);
     // make a request to the url
     fetch(apiUrl).then(function(response) {
         if(response.ok) {
             response.json().then(function(data) {
-            displayCity(data, city);
-          });
+                console.log(data);
+            displayCity(data,city);
+            });
         } else {
             alert("Error: City Not Found");
         }
@@ -33,7 +36,25 @@ var getCityWeather = function(city) {
         // Notice this `.catch()` getting chained onto the end of the `then()` method
         alert("Unable to connect to Weather");
     });
+};
+
+var getCityLocation = function (city) {
+   console.log("this is working");
+    var geoApiUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=1&appid=526fc11b10a27117543151b1de2f92b6";
+    console.log(city);
     
+    fetch(geoApiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+            console.log(data);
+                var newLat = data[0].lat;
+                var newLon = data[0].lon;
+            getCityWeather(newLon, newLat,city);
+            });
+        } else {
+            alert("Error: City Not Found");
+        }
+    })
 };
 
 var formSubmitHandler = function(event) {
@@ -43,7 +64,7 @@ var formSubmitHandler = function(event) {
     var city = cityInputEl.value.trim();
     console.log(city);
     if (city) {
-        getCityWeather(city);
+        getCityLocation(city);
         cityInputEl = "";
 
     } else {
@@ -52,31 +73,66 @@ var formSubmitHandler = function(event) {
 };
 
 var displayCity = function(data, citySearchTerm) {
-    console.log(data);
-    console.log(citySearchTerm);
+        console.log(data);
+        console.log(citySearchTerm);
 
-    // clear old content
-    cityContainerEl.textContent = "";
-    citySearchTerm.textContent = citySearchTerm;
+     // clear old content
+     cityContainerEl.textContent = "";
+     newCitySearchTerm.textContent = citySearchTerm;
 
     //display info
-    var newCityTemp = Number(data.main.temp);
-    console.log(newCityTemp);
-    //how do I get the date?
-    var dateTemp = Number(data.dt);
-    console.log(dateTemp);
-    var newHumidity = Number(data.main.humidity);
-    console.log(newHumidity);
-    var newWindSpeed = Number(data.wind.speed);
-    console.log(newWindSpeed);
-    var newIcon = src=" http://openweathermap.org/img/wn/" +data.weather[0].icon;
-    console.log(newIcon);
-    var newLat = Number(data.coord.lat);
-    console.log(newLat);
-    var newLon = Number(data.coord.lon);
-    console.log(newLon);
-
+     // display city name
     
+     
+     var cityP = document.createElement("p");
+     cityP.innerHTML = citySearchTerm;
+     cityContainerEl.appendChild(cityP);
+
+    // current temp
+    var newCityTemp = data.current.temp;
+    console.log(newCityTemp);
+    var tempP = document.createElement("p");
+    tempP.innerHTML = "Current Temp: " + newCityTemp +" degrees F";
+    cityContainerEl.appendChild(tempP);
+//     // how do I get the date?
+    
+//     var dateTemp = data.dt;
+//     console.log(dateTemp);
+    
+     // current humidity
+    var newHumidity = data.current.humidity;
+    console.log(newHumidity);
+    var humidityP = document.createElement("p");
+    humidityP.innerHTML = "Humdity: " +newHumidity + "%";
+    cityContainerEl.appendChild(humidityP);
+
+     // current wind speed
+    var newWindSpeed = data.current.wind_speed;
+    console.log(newWindSpeed);
+    var windP = document.createElement("p");
+    windP.innerHTML = "Wind Speed: " + newWindSpeed + " MPH"; 
+    cityContainerEl.appendChild(windP);
+
+    // UV index
+    var newUV = data.current.uvi;
+    console.log(newUV);
+    var uvP = document.createElement("p");
+    uvP.innerHTML = "UV Index: " + newUV;
+    cityContainerEl.appendChild(uvP);
+
+    if (newUV <2) {
+        uvP.setAttribute("style", "background-color: green");
+    } else if ( newUV >2 && newUV < 5) {
+        uvP.setAttribute("style", "background-color: yellow");
+    } else {
+        uvP.setAttribute("class", "background-color: red");
+    }
+    // // icon
+    // var newIcon = data.current.weather[0].icon;
+    // console.log(newIcon);
+    // document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+newIcon+".png";
+
+
 
 };
 
